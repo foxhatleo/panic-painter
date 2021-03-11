8b853f7222938dd01e206bd95acec78b4428757e
Laura Sizemore <ls797@cornell.edu>
2021-03-10 23:00:00 -0500

    fixed logic

--- source/scenes/gameplay/PPGameScene.cpp
+++ source/scenes/gameplay/PPGameScene.cpp
@@ -62,16 +62,21 @@ void GameScene::update(float timestep) {
     _state.update(timestep);
     for (uint i = 0, j = _state.numQueues(); i < j; i++) {
         for (uint i2 = 0, j2 = _state.numCanvases(i); i2 < j2; i2++) {
-            if (input.moved()) {
-                if (InputController::inScene(input.startingPoint(),
-                    _canvases[i][i2]->getInteractionNode()) &&
-                    (input.startingPoint().x + input.movedDist().x) > _canvases[i][i2]->getWidth()) {
-                    //Drag
-                    _canvases[i][i2]->update(DONE, _state.getColorsOfCanvas(i, i2),
-                        _state.getTimer(i, i2));
+            auto state = _state.getCanvasState(i, i2);
+            if (state == ACTIVE && input.moved()) {
+                float distDragged = input.startingPoint().x + input.movedDist().x;
+                if (distDragged > _canvases[i][i2]->getInteractionNode()->getPositionX() ||
+                    (InputController::inScene(input.currentPoint(), _canvases[i][i2]->getInteractionNode())
+                        && input.currentPoint() != input.startingPoint())) {
+                    CULog("success drag");
+                        //Drag success
+                        //TODO add logic
                 }
             }
-            auto state = _state.getCanvasState(i, i2);
+            if(state == ACTIVE && input.completeHold() && (InputController::inScene(input.currentPoint(), _canvases[i][i2]->getInteractionNode())
+                && input.currentPoint() == input.startingPoint())){
+                CULog("success hold");
+            }
             _canvases[i][i2]->update(
                     state,
                     _state.getColorsOfCanvas(i, i2),
