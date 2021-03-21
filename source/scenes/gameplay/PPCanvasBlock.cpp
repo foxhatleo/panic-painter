4074277549ca779f925b3600d8c2916db0b43e08
Benjamin Gillott <bg357@cornell.edu>
2021-03-21 16:17:26 -0400

    Added panda texture, realigned timer and color indicators to be more visible

--- source/scenes/gameplay/PPCanvasBlock.cpp
+++ source/scenes/gameplay/PPCanvasBlock.cpp
@@ -14,20 +14,38 @@ ptr<CanvasBlock> CanvasBlock::alloc(const asset_t& assets,
 
 void CanvasBlock::_setup(const asset_t& assets,
     const vec<Color4>& colors) {
-    // White background
-    _bg = scene2::PolygonNode::alloc(Rect(0, 0, getWidth(), getHeight()));
+    
+    //Rectangle only version
+    //_bg = scene2::PolygonNode::alloc(Rect(0, 0, getWidth(), getHeight()));
+    
+    //Load in the panda texture from scene and attach to a new polygon node
+    _bg = scene2::PolygonNode::allocWithTexture(assets->get<Texture>("panda"));
     _bg->setColor(Color4::WHITE);
+    
+    _bg->setAnchor(Vec2::ANCHOR_BOTTOM_CENTER);
+    //TODO: Adjust the scale
+    float scale = 0.13f;
+    int sprite_width = (int) 1208 * scale;
+    int sprite_height = (int) 2112 * scale;
+    
+    //Do we need both?
+    setContentSize(sprite_width, sprite_height);
+    _bg->setContentSize(sprite_width, sprite_height);
+    _bg->setPosition(getWidth() / 2, 0);
     addChild(_bg);
-
+    
+    
     // Color strip
     _colorStrip = ColorStrip::alloc(getWidth(), colors);
+    _colorStrip->setPosition(getWidth() / 2, getHeight()/2 - 15);
     addChild(_colorStrip);
 
+    
     // Timer label
     _timerText = scene2::Label::alloc("", assets->get<Font>("roboto"));
     _timerText->setHorizontalAlignment(scene2::Label::HAlign::CENTER);
     _timerText->setVerticalAlignment(scene2::Label::VAlign::BOTTOM);
-    _timerText->setPosition(getWidth() / 2, 5);
+    _timerText->setPosition(getWidth() / 2, 35);
     addChild(_timerText);
 
     _hoverAllowed = true;
