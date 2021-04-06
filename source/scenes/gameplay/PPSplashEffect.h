b8bdc2508cb6b0dccf20284212b83d55e5a59a1a
Laura Sizemore <ls797@cornell.edu>
2021-04-06 16:43:53 -0400

    add new splasheffect class

---
+++ source/scenes/gameplay/PPSplashEffect.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef PANICPAINTER_PPSPLASHEFFECT_H
+#define PANICPAINTER_PPSPLASHEFFECT_H
+
+#include "utils/PPHeader.h"
+#include "controllers/PPInputController.h"
+#include "controllers/PPGameStateController.h"
+#include "utils/PPAnimation.h"
+#include "PPCanvas.h"
+#include "PPColorPalette.h"
+#include "PPGlobalTimer.h"
+#include "controllers/PPActionController.h"
+class SplashEffect
+{
+};
+
+#endif /* PP_COLOR_PALETTE_H */
