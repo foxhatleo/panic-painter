1c2e9f276257967a35342d6f9f7ca638ad0b3644
Laura Sizemore <ls797@cornell.edu>
2021-04-06 20:05:19 -0400

    added basic .h file

--- source/scenes/gameplay/PPSplashEffect.h
+++ source/scenes/gameplay/PPSplashEffect.h
@@ -5,13 +5,26 @@
 #include "utils/PPHeader.h"
 #include "controllers/PPInputController.h"
 #include "controllers/PPGameStateController.h"
-#include "utils/PPAnimation.h"
 #include "PPCanvas.h"
-#include "PPColorPalette.h"
-#include "PPGlobalTimer.h"
-#include "controllers/PPActionController.h"
-class SplashEffect
+class SplashEffect: public SceneNode
 {
+protected:
+	//Likely not useful now, but may be necessary when getting actual paint textures later on
+	/*Additionally, we likely do not need InputController and can do this instead using 
+	GameState/actionController. Including it just in case we don't want to route through GameState*/
+	const asset_t& _assets;
+	const Color4 _selectedColor; 
+	const InputController& _input; 
+
+public:
+	/** Allocate canvas block with a particular length on the side. This can also be a Rect bounds if easier */
+	static ptr<SplashEffect> alloc(const asset_t& assets,
+		float size,
+		const Color4 colors, const InputController& input);
+	void update(); 
+
+	void draw(const std::shared_ptr<SpriteBatch>& batch, const Mat4& transform, Color4 tint) override; 
+
 };
 
-#endif /* PP_COLOR_PALETTE_H */
+#endif /* PP_SPLASH_EFFECT_H */
