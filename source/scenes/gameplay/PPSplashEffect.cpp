64221210dc03855d5ace34ecdde770404e4f32ef
Wenhao Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2021-04-09 01:53:29 +0800

    Add input sampling in splash effect

--- source/scenes/gameplay/PPSplashEffect.cpp
+++ source/scenes/gameplay/PPSplashEffect.cpp
@@ -1,1 +1,25 @@
 #include "PPSplashEffect.h"
+
+void SplashEffect::update(float timestep, Color4 currentColor) {
+    // Decrease opacity of all points by a bit.
+    for (auto &i : _queue) {
+        i.color.a -= (timestep / FADE_DURATION) * 255;
+        if (i.color.a < 0) i.color.a = 0;
+    }
+
+    // Reset ticker if no input.
+    if (!_input.isPressing()) { ticker = 0; } else {
+        ticker++;
+        if (ticker == SAMPLE_RATE) {
+            ticker = 0;
+            // Time to sample!
+            // First push everything in the point array back. The last one is
+            // discarded.
+            for (uint i = QUEUE_MAX_SIZE - 1; i >= 1; i--)
+                _queue[i] = _queue[i - 1];
+            // Update the point and color.
+            _queue[0].point = _input.currentPoint();
+            _queue[0].color = currentColor;
+        }
+    }
+}
--- source/scenes/gameplay/PPSplashEffect.h
+++ source/scenes/gameplay/PPSplashEffect.h
@@ -6,22 +6,38 @@
 #include "controllers/PPInputController.h"
 #include "controllers/PPGameStateController.h"
 #include "PPCanvas.h"
+
+#define QUEUE_MAX_SIZE 4
+#define SAMPLE_RATE 4
+#define FADE_DURATION 1
+
 class SplashEffect: public SceneNode
 {
 protected:
 	//Likely not useful now, but may be necessary when getting actual paint textures later on
 	/*Additionally, we likely do not need InputController and can do this instead using 
 	GameState/actionController. Including it just in case we don't want to route through GameState*/
-	const asset_t& _assets; 
+	const asset_t &_assets;
 
 	/*Other alternative is to just use gameState or grab all needed values here*/
-	const InputController& _input; 
+	const InputController &_input;
+
+	struct {
+	    Vec2 point;
+	    Color4 color;
+	} _queue[QUEUE_MAX_SIZE];
+
+	uint ticker;
 
 public:
+    explicit SplashEffect(const asset_t& assets) :
+    _assets(assets), _input(InputController::getInstance()) {}
+
 	/** Allocate canvas block with a particular length on the side. This can also be a Rect bounds if easier */
-	static ptr<SplashEffect> alloc(const asset_t& assets,
-		float size, const InputController& input);
-	void update(Color4 currentColor); 
+	static ptr<SplashEffect> alloc(
+	    const asset_t& assets,
+		float size);
+	void update(float timestep, Color4 currentColor);
 
 	void draw(const std::shared_ptr<SpriteBatch>& batch, const Mat4& transform, Color4 tint) override; 
 
