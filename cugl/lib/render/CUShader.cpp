ec9cd78fa0cab829ef8bedc89dfa5d66ab2c365a
Wenhao Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2021-04-11 03:37:27 +0800

    Reverse unnecessary changes in alpha-presentation

--- cugl/lib/render/CUShader.cpp
+++ cugl/lib/render/CUShader.cpp
@@ -175,13 +175,13 @@ void Shader::logShaderError(GLuint shader) {
     //Shader log length
     int infoLogLength = 0;
     int maxLength = infoLogLength;
-
+        
     //Get info string length
     glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &maxLength );
-
+        
     //Allocate string
     char* infoLog = new char[ maxLength ];
-
+        
     //Get info log
     glGetShaderInfoLog( shader, maxLength, &infoLogLength, infoLog );
     if( infoLogLength > 0 ) {
