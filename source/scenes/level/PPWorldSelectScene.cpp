681231e0be8aac32dcc81531f7d4a57dc5860aca
Bryan Graeser <bsg68@cornell.edu>
2021-05-04 13:45:32 -0400

    fixed for world select

--- source/scenes/level/PPWorldSelectScene.cpp
+++ source/scenes/level/PPWorldSelectScene.cpp
@@ -8,18 +8,21 @@ bool WorldSelectScene::init(const asset_t &assets) {
     _safe = Application::get()->getSafeBounds();
     _sceneSize = _safe.size;
     _offsetInSafe = Vec2::ZERO;
-
+    Size scale = _safe.size;
     if (_sceneSize.width / SCENE_SIZE_W > _sceneSize.height / SCENE_SIZE_H) {
-        _sceneSize *= (SCENE_SIZE_H / _sceneSize.height);
-    } else {
-        _sceneSize *= (SCENE_SIZE_W / _sceneSize.width);
+        scale *= (SCENE_SIZE_H / _sceneSize.height);
     }
+    else {
+        scale *= (SCENE_SIZE_W / _sceneSize.width);
+    }
+    _sceneSize = Application::get()->getDisplaySize();
 
     if (assets == nullptr) {
         return false;
     } else if (!Scene2::init(_sceneSize)) {
         return false;
     }
+    
 
     _assets = assets;
     _assets->loadDirectory("scenes/worldselect.json");
@@ -59,14 +62,32 @@ void WorldSelectScene::activateUI(
         scene);
     if (button != nullptr) {
 //        CULog("Activating button %s", button->getName().c_str());
+        // Set button sizing
+        Size scale = _safe.size;
+        if (_sceneSize.width / SCENE_SIZE_W > _sceneSize.height / SCENE_SIZE_H) {
+            scale *= (SCENE_SIZE_H / _sceneSize.height);
+        }
+        else {
+            scale *= (SCENE_SIZE_W / _sceneSize.width);
+        }
+
         if (button->getName() == "menubutton") {
-            button->setPosition(_offsetInSafe.x, _safe.size.height - _offsetInSafe.y);
+            button->setScale(button->getScale() *
+                _safe.size.height / SCENE_SIZE_H);
+            //button->setPosition(_offsetInSafe.x, _safe.size.height - _offsetInSafe.y);
+            button->setAnchor(Vec2::ANCHOR_TOP_LEFT);
+            button->setPosition(0, _offsetInSafe.y + _safe.size.height);
+            //button->setPosition(_safe.size.width-button->getWidth(), _safe.size.height-button->getHeight());
             button->addListener([=](const string &name, bool down) {
                 if (!down) {
                     _state = BACK;
                 }
             });
         } else {
+            button->setScale(button->getScale() *
+                _safe.size.height / SCENE_SIZE_H);
+            button->setContentSize(scale);
+
             // TODO: REMOVE CONTENT BELOW WHEN ALL WORLDS ARE DONE.
             if (button->getName() != "museum" &&
                 button->getName() != "city" &&
