749e2872a0e5fdd49f1b21fec1f38ab44b4a0bae
Wenhao (Leo) Liang <36321327+foxhatleo@users.noreply.github.com>
2021-05-19 21:52:39 +0800

    Add hacking mechanism

--- source/scenes/menu/PPMenuScene.cpp
+++ source/scenes/menu/PPMenuScene.cpp
@@ -5,6 +5,7 @@
 void MenuScene::dispose() {
     deactivateUI(_scene);
     Scene2::dispose();
+    _hackTimer = nullptr;
 }
 
 bool MenuScene::init(const asset_t &assets) {
@@ -31,6 +32,8 @@ bool MenuScene::init(const asset_t &assets) {
     _scene = _assets->get<scene2::SceneNode>("menuscene");
     _scene->setContentSize(screenSize);
     _scene->doLayout(); // Repositions the HUD
+
+    _hackTimer = make_shared<Timer>(3);
 
     // Initialize background
     auto menuBackground = PolygonNode::allocWithTexture(_assets->get<Texture>
@@ -70,7 +73,10 @@ MenuScene::activateUI(const std::shared_ptr<cugl::scene2::SceneNode> &scene) {
                 //CULog("LEVEL STATUS");
                 if (!down) {
                     SoundController::getInstance()->playSfx("button");
+                    _hacking = false;
                     this->_state = LEVELS;
+                } else {
+                    _hacking = true;
                 }
             });
         }
@@ -113,6 +119,29 @@ void MenuScene::resetState() {
 
 void MenuScene::update(float timestep) {
     SoundController::getInstance()->useBgm("menu");
+    if (_hacking) {
+        CULog("Hacking in progress!");
+        _hackTimer->update(timestep);
+    }
+    if (_hackTimer->finished()) {
+        CULog("Hacked!");
+        string levels[] = {
+            "city-1", "city-2", "city-3", "city-4", "city-5",
+            "eiffel-1", "eiffel-2", "eiffel-3", "eiffel-4", "eiffel-5",
+            "house-1", "house-2", "house-3", "house-4", "house-5",
+            "island-1", "island-2", "island-3", "island-4", "island-5",
+            "museum-1", "museum-2", "museum-3", "museum-4", "museum-5",
+            "space-1", "space-2", "space-3", "space-4", "space-5",
+        };
+        uint levelsLen = 5 * 6;
+        for (uint i = 0; i < levelsLen; i++) {
+//            SaveController::getInstance()->unlock(levels[i]);
+        }
+        Animation::set(_scene, {{"opacity", 0}});
+        Animation::to(_scene, .2, {{"opacity", 1}});
+        _hackTimer->reset();
+        _hacking = false;
+    }
 }
 
 MenuRequest MenuScene::getState() const {
