d263306c084e37cf7050a0e405d2cb1bbfeab8b3
Bryan Graeser <bsg68@cornell.edu>
2021-05-23 12:09:09 -0400

    bomb tutorial complete

--- source/scenes/gameplay/PPGameScene.cpp
+++ source/scenes/gameplay/PPGameScene.cpp
@@ -168,6 +168,7 @@ void GameScene::loadLevel(const string &levelName) {
         addChild(_tutorialOverlay);
     }
 
+
     _action = make_shared<ActionController>(_state, _canvases);
 
     addChild(_backBtn);
